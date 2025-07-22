9e0cd46dd3842f2457ceab415de4a2cdda8d1798
shukant75 <spanjikar9155@gmail.com>
2025-07-22 18:17:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.modle=modle;
+}
 
-function SportsCar(make, model, topSpeed) {}
+
+function SportsCar(make, model, topSpeed) {
+	call.car(make,modle,topSpeed)
+	this.topSpeed=topSpeed
+}
 
 // Do not change the code below
 window.Car = Car;
